7710e81ee6e84e7ce6d5dbd05525755714e76680
yunseul <siddl333@naver.com>
2024-08-28 10:52:51 +0900

    [Fix] 충돌 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome!! to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-=======
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <TestComponent></TestComponent>
@@ -16,20 +7,15 @@ import HelloWorld from './components/HelloWorld.vue'
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import TestComponent from "@/components/TestComponent.vue";
->>>>>>> Stashed changes
+
 export default {
   name: 'App',
   components: {
-<<<<<<< Updated upstream
-    HelloWorld
-  }
-}
-=======
     HelloWorld,
     TestComponent
   },
 };
->>>>>>> Stashed changes
+
 </script>
 
 <style>
