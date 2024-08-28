572baa42836eb52a8e943a922c1562ec98685d77
hyejeung <rnvl1209@skuniv.ac.kr>
2024-08-28 10:26:06 +0900

    [Feat] Testcomponent 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome!! to Your Vue.js App"/>
 </template>
@@ -6,12 +7,29 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+=======
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <TestComponent></TestComponent>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TestComponent from "@/components/TestComponent.vue";
+>>>>>>> Stashed changes
 export default {
   name: 'App',
   components: {
+<<<<<<< Updated upstream
     HelloWorld
   }
 }
+=======
+    HelloWorld,
+    TestComponent
+  },
+};
+>>>>>>> Stashed changes
 </script>
 
 <style>
