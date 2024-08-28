3df80ed21c5050ca817b3db8c2a749dd3d530273
hyejeung <rnvl1209@skuniv.ac.kr>
2024-08-28 14:04:00 +0900

    [Refactor] log added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome!! to Your Vue.js App" />
-  <TestComponent></TestComponent>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import TestComponent from "@/components/TestComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    TestComponent
   },
+  created() {
+    console.log("v1");
+  },
+  computed: {},
 };
 
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
