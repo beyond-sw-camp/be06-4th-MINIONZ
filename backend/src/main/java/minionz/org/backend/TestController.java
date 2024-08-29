e11b7110a87c68ba4995fb50f747b83b92447967
yunseul <siddl333@naver.com>
2024-08-29 09:27:18 +0900

    [Refactor] 테스트용 version 변경

--- backend/src/main/java/minionz/org/backend/TestController.java
+++ backend/src/main/java/minionz/org/backend/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
-        return "version: 01 ";
+        return "version: 02 ";
     }
 }
